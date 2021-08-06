220cddf3dfaa9749c86cf7e1d55ebc6eab87252d
migu-chen <w123640a@gmail.com>
2021-08-06 12:41:10 +0800

    log write

--- WebformMiniSample/AccountingNote.DBsource/logger.cs
+++ WebformMiniSample/AccountingNote.DBsource/logger.cs
@@ -8,9 +8,12 @@ namespace AccountingNote.DBsource
 {
     public  class logger
     {
-        public static void WriteLog(Exception es)
+        public static void WriteLog(Exception ex)
         {
-
+            string Msg=
+                $@"                  ":
+            System.IO.File.AppendAllText("D:\\logs\\logs.log", ex.ToString());
+            throw ex;
         }
     }
 }
